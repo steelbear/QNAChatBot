7da932044605f1a110362009b4599c7613c0a8a4
Cheolung Choi <choisteelbear@gmail.com>
2024-01-06 10:37:41 +0900

    3. Handling Connection Errors

--- ui/main.py
+++ ui/main.py
@@ -1,5 +1,3 @@
-import time
-
 import requests
 import streamlit as st
 
@@ -18,17 +16,28 @@ def update_messages_and_status():
     st.session_state['chat_running'] = True
 
 
-def request_to_server():
-    response = requests.post('http://localhost:8000/api/ask',
-                             json={'question': st.session_state['question']},
-                             ).json()
-
-    print(response)
-    
-    st.session_state['chat_messages'].append({
-        'role': 'assistant',
-        'content': response['content']
-    })
+def request_to_server(question: str):
+    try:
+        response = requests.post('http://localhost:8000/api/ask',
+                                json={'question': question},
+                                )
+        response.raise_for_status()
+        response = response.json()
+    except requests.ConnectionError:
+        st.error('Failed to connect to the server. Try again.')
+        st.button('Retry')
+        st.session_state['question'] = question
+        st.stop()
+    except requests.HTTPError:
+        st.error(str(response.status_code) + ' ' + response.reason)
+        st.button('Retry')
+        st.session_state['question'] = question
+        st.stop()
+    else:
+        st.session_state['chat_messages'].append({
+            'role': 'assistant',
+            'content': response['content']
+        })
 
 
 # chat messages initialization
@@ -49,18 +58,18 @@ for message in st.session_state['chat_messages']:
     with st.chat_message(message['role']):
         st.write(message['content'])
 
+# request the answer to the model server
+if st.session_state['chat_running']:
+    with st.chat_message('assistant'):
+        with st.spinner('Thinking...'):  # show the status requesting the answer
+            request_to_server(st.session_state['question'])
+            # change chat status after gotten response
+            st.session_state['chat_running'] = False
+            # rerun to show the answer
+            st.rerun()
+
 # show chat input widget
 question = st.chat_input(key='question',
                          on_submit=update_messages_and_status,
                          disabled=st.session_state['chat_running'],
                          )
-
-# request the answer to the model server
-if st.session_state['chat_running']:
-    with st.chat_message('assistant'):
-        with st.spinner('Thinking...'):  # show the status requesting the answer
-            request_to_server()
-            # change chat status after gotten response
-            st.session_state['chat_running'] = False
-            # rerun to show the answer
-            st.rerun()
